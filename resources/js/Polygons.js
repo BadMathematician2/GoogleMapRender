0afa2ebde1c92cbe5ff21889388500a919cb9bd6
Mykola <mathematic204chnu@gmail.com>
2020-09-07 17:49:31 +0300

    added zoom and center for all polygons

--- resources/js/Polygons.js
+++ resources/js/Polygons.js
@@ -3,7 +3,7 @@ class Polygons extends BaseMap {
     renderPolygons() {
         this.request((data) => {
             let polygons = JSON.parse(data)
-            this.getMap().setCenter(polygons[0].points[0])
+            this.zoomAndCenter(polygons)
             polygons.map(polygon => {
                 (polygon.points.length !== 2) ? this.newPolygon(polygon) : this.newRectangle(polygon)
             })
@@ -37,5 +37,32 @@ class Polygons extends BaseMap {
                 west: polygon.points[0].lng
             }
         })
+    }
+
+    zoomAndCenter(polygons) {
+        let minLat = polygons[0].points[0].lat
+        let maxLat = polygons[0].points[1].lat
+        let minLng = polygons[0].points[0].lng
+        let maxLng = polygons[0].points[1].lng
+        const max = (x, y) => x > y ? x : y
+        const min = (x, y) => x < y ? x : y
+
+        polygons.map(polygon => {
+            polygon.points.map(p => {
+                minLat = min(minLat, p.lat)
+                maxLat = max(maxLat, p.lat)
+                minLng = min(minLng, p.lng)
+                maxLng = max(maxLng, p.lng)
+            })
+
+        })
+
+        let bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(minLat,minLng),
+            new google.maps.LatLng(maxLat, maxLng)
+        )
+        this.getMap().setCenter(bounds.getCenter())
+        this.getMap().fitBounds(bounds)
+
     }
 }
